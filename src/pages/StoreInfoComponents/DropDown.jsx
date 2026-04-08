a5b3d2062a14e4cf2c9dd84ea8ad1b88643f8d11
minjung1ee <lminjung9996@gmail.com>
2026-04-08 18:14:35 +0900

    매장안내 데이터, 컴포넌트 추가

---
+++ src/pages/StoreInfoComponents/DropDown.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function DropDown() {
+    return (
+        <div>
+            <select name="selectedArea1">
+
+            </select>
+        </div>
+    );
+}
---
+++ src/pages/StoreInfoComponents/LocationSearch.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function LocationSearch() {
+    return (
+        <div className="store-location-search">
+            <form action="" className="store-search-form">
+                <div className="location">
+                    {/* dropdown 컴포넌트 넣기 */}
+                    <div className="selector area1">selector area1</div>
+                    <div className="selector area2">selector area2</div>
+                </div>
+            </form>
+        </div>
+    );
+}
