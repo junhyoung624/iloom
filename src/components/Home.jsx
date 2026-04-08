395fec0aa6afc0b09a75798b5ad046324821649d
sungmin <okok0216@gmail.com>
2026-04-08 13:00:48 +0900

    Accordion,Magazine,Sns,Series

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import FooterAccordion from './FooterAccordion'
+import Magazine from './Magazine'
+import Sns from './Sns'
+import Series from './Series'
+
+
 
 const Home = () => {
   return (
     <div className='main-video'>
-      <img src="" alt="" />
-      <img src="" alt="" />
-
+      <img src="./images/mainImg.png" alt="" />
+      <Series />
+      <Sns />
+      <Magazine />
+      <FooterAccordion />
     </div>
   )
 }
