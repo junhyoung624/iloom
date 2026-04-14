d7a02c81c5964cae34a253e92a05fb7b17a617c3
minjung1ee <lminjung9996@gmail.com>
2026-04-14 14:37:08 +0900

    space coordi 진행중

--- src/components/ImageWithTags.jsx
+++ src/components/ImageWithTags.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./scss/ImageWithTags.scss";
 import { spaceCoordiData } from '../data/spaceCoordiData.js';
@@ -6,25 +6,54 @@ import { spaceCoordiData } from '../data/spaceCoordiData.js';
 export default function ImageWithTags() {
     const tab_menu = spaceCoordiData.map((item) => item.tab);
     const [selectedTab, setSelectedTab] = useState("tab3");
+
     const currentObject = spaceCoordiData.filter((item) => item.tab === selectedTab);
     const currentProductList = currentObject[0].products;
+    const allIdArr = currentProductList.map((item) => item.id);
+    const defaultId = currentProductList[0].id;
 
-    const [shopBtn, setShopBtn] = useState(false);
+
+    //현재 보이는 태그들 id만 저장
+    const [visibleTags, setVisibleTags] = useState([defaultId]);
+
+    useEffect(() => {
+        const defaultItem = currentProductList.filter((item) => item.default).map((item) => item.id);
+        setVisibleTags(defaultItem);
+    }, [selectedTab]);
 
     const handleTabBtn = (tab) => {
         console.log("tab btn in");
         setSelectedTab(tab);
-        setShopBtn(false);
+        console.log(currentProductList);
+        //setVisibleTags([currentProductList[0].id]); //tab 변경시 초기화
     }
 
     const handleShopBtn = () => {
-        console.log("btn in");
+        const isAllSelected = visibleTags.length === allIdArr.length;
 
-        setShopBtn(!shopBtn);
-        console.log(shopBtn);
+        if (isAllSelected) {
+            setVisibleTags([]);
+        } else {
+            setVisibleTags(allIdArr);
+        }
     }
 
-    console.log(tab_menu);
+    const handleDot = (itemId) => {
+
+        setVisibleTags((prev) => {
+            if (prev.includes(itemId)) {
+                //이미 보이는 상태면 숨기기
+                return prev.filter((id) => id !== itemId);
+            } else {
+                //안 보이면 추가
+                return [...prev, itemId];
+            }
+
+        })
+
+    }
+    const isAllSelected = visibleTags.length === allIdArr.length;
+
     return (
         <div className='shoppable-area-wrap'>
             <div className="tab-menu">
@@ -43,54 +72,59 @@ export default function ImageWithTags() {
                 <div className="dot-and-box-wrap">
                     <ul>
                         {
-                            currentProductList.map((item, id) =>
-                                <li key={id}
-                                    style={{ position: "absolute", left: `${item.position.x / 1440 * 100}%`, top: `${item.position.y / 799 * 100}%` }}>
-                                    <div className="dot-area">
-                                        {/* dot {item.position.x} {item.position.y} */}
-                                    </div>
-                                    <div className={shopBtn ? "price-tag-active" : "price-tag-hidden"} >
-                                        {/* price tag area */}
-                                        <div className="tag-inner"
-                                            style={{ position: "absolute", transform: `${item.tag_position_code}` }}>
-                                            <div className="img-info">
-                                                <img src={item.src} alt="." />
+                            currentProductList.map((item, id) => {
+                                const isVisible = visibleTags.includes(item.id);
+
+                                return (
+                                    <li key={id}
+                                        style={{
+                                            position: "absolute",
+                                            left: `${item.position.x / 1440 * 100}%`,
+                                            top: `${item.position.y / 799 * 100}%`
+                                        }}>
+                                        <div className="dot-area"
+                                            onClick={() => handleDot(item.id)}>
+                                            {/* dot {item.position.x} {item.position.y} */}
+                                        </div>
+                                        <div className={isVisible ? "price-tag-active" : "price-tag-hidden"}  >
+                                            {/* price tag area */}
+                                            <div className="tag-inner"
+                                                style={{
+                                                    position: "absolute",
+                                                    transform: `${item.tag_position_code}`
+                                                }}>
+                                                <div className="img-info">
+                                                    <img src={item.src} alt="." />
+                                                </div>
+                                                <div className="txt-info">
+                                                    <p className='item-name'>{item.name}</p>
+                                                    <p className='item-subname'>{item.subName}</p>
+                                                    <p className='item-price'>{item.price}</p>
+                                                </div>
+                                                <div className="button-area">
+                                                    <p>::</p>
+                                                </div>
                                             </div>
-                                            <div className="txt-info">
-                                                <p className='item-name'>{item.name}</p>
-                                                <p className='item-subname'>{item.subName}</p>
-                                                <p className='item-price'>{item.price}</p>
-                                            </div>
-                                            <div className="button-area">
-                                                <p>::</p>
-                                            </div>
+
                                         </div>
 
-                                    </div>
+                                    </li>
 
-                                </li>
+                                )
+                            }
 
                             )
                         }
                     </ul>
                     <div className="shop-btn-area"
                         onClick={handleShopBtn}>
-                        <img src="./images/spaceCoordi/shop-btn.png" alt="." />
+                        <img
+                            src="./images/spaceCoordi/shop-btn.png"
+                            alt="."
+                            className={isAllSelected ? "shop-btn-active" : ""} />
                     </div>
                 </div>
             </Link >
-
-            {/* 제품 개수만큼 map 돌려서 li생성 */}
-
-
-            {/* <div className="dot-and-box-wrap">
-                <ul area-hidden={false}>
-                    <li>
-                        <a className='dot-area'>dot</a>
-                        <div className="price-tag"></div>
-                    </li>
-                </ul>
-            </div> */}
         </div >
     );
 }
