3794c9b5376315817b39ec238d0d29e0cbb5f348
sungmin <okok0216@gmail.com>
2026-04-14 14:19:32 +0900

    search

--- src/components/HeaderInner.jsx
+++ src/components/HeaderInner.jsx
@@ -5,12 +5,12 @@ import { useAuthStore } from '../store/useAuthStore'
 import "./scss/headerinner.scss"
 import SearchDropdown from './SearchDropdown'
 
-const HeaderInner = ({ onEnter, userClick, isHover }) => {
+const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick }) => {
     const { menus } = useProductStore()
     const { user } = useAuthStore()
-    const [isSearchOpen, setIsSearchOpen] = useState(false)
+
     const [lastScroll, setLastScroll] = useState(false)
-    const searchRef = useRef(null)
+
 
     useEffect(() => {
         let lastScrollY = window.scrollY
@@ -31,100 +31,77 @@ const HeaderInner = ({ onEnter, userClick, isHover }) => {
         return () => window.removeEventListener("scroll", AddMenuScroll)
     }, [])
 
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            if (searchRef.current && !searchRef.current.contains(e.target)) {
-                setIsSearchOpen(false)
-            }
-        }
-
-        document.addEventListener("mousedown", handleClickOutside)
-        return () => document.removeEventListener("mousedown", handleClickOutside)
-    }, [])
-
-    const handleSearchToggle = () => {
-        setIsSearchOpen((prev) => !prev)
-    }
-
     return (
         <>
-            <div
-                className={`search-overlay ${isSearchOpen ? "active" : ""}`}
-                onClick={() => setIsSearchOpen(false)}
-            />
-            <div
-                className={`header-inner-wrap ${isSearchOpen ? "search-open" : ""}`}
-                ref={searchRef}
-            >
-                <div className="header-inner">
-                    <div className="header-left">
-                        <div className="ham-btn" onMouseEnter={onEnter}>
-                            <img src="./images/logo-icon/ham-white.png" alt="ham-btn" />
-                        </div>
-                    </div>
 
-                    <h1 className="main-logo">
-                        <Link to="/"><img src="./images/logo-icon/main-logo-white.png" alt="" /></Link>
-                    </h1>
 
-                    <div className="header-right">
-                        <ul className="gnb-list">
-                            {user ? (
-                                <>
-                                    <li>
-                                        <button
-                                            type="button"
-                                            className="icon-btn"
-                                            onClick={handleSearchToggle}
-                                        >
-                                            <img src="./images/logo-icon/search-white.png" alt="search" />
-                                        </button>
-                                    </li>
-                                    <li>
-                                        <button className='user-btn' onClick={userClick}>
-                                            <img src="./images/logo-icon/user-white.png" alt="" />
-                                            <span>{user.name}</span>
-                                        </button>
-                                    </li>
-                                    <li>
-                                        <Link to="/cart">
-                                            <img src="./images/logo-icon/cart-white.png" alt="" />
-                                            <span className="cart-num">0</span>
-                                        </Link>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    <li>
-                                        <button
-                                            type="button"
-                                            className="icon-btn"
-                                            onClick={handleSearchToggle}
-                                        >
-                                            <img src="./images/logo-icon/search-white.png" alt="search" />
-                                        </button>
-                                    </li>
-                                    <li>
-                                        <Link to="/login"><img src="./images/logo-icon/user-white.png" alt="" /></Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/cart">
-                                            <img src="./images/logo-icon/cart-white.png" alt="" />
-                                            <span className="cart-num">0</span>
-                                        </Link>
-                                    </li>
-                                </>
-                            )}
-                        </ul>
+            <div className="header-inner">
+                <div className="header-left">
+                    <div className="ham-btn" onMouseEnter={onEnter}>
+                        <img src="./images/logo-icon/ham-white.png" alt="ham-btn" />
                     </div>
                 </div>
 
-                <div className={`search-dropdown ${isSearchOpen ? "active" : ""}`}>
-                    <SearchDropdown />
+                <h1 className="main-logo">
+                    <Link to="/"><img src="./images/logo-icon/main-logo-white.png" alt="" /></Link>
+                </h1>
+
+                <div className="header-right">
+                    <ul className="gnb-list">
+                        {user ? (
+                            <>
+                                <li>
+                                    <button
+                                        type="button"
+                                        className="icon-btn"
+                                        onClick={searchClick}
+                                    >
+                                        <img src="./images/logo-icon/search-white.png" alt="search" />
+                                    </button>
+                                </li>
+                                <li>
+                                    <button className='user-btn' onClick={userClick}>
+                                        <img src="./images/logo-icon/user-white.png" alt="" />
+                                        <span>{user.name}</span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <Link to="/cart">
+                                        <img src="./images/logo-icon/cart-white.png" alt="" />
+                                        <span className="cart-num">0</span>
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li>
+                                    <button
+                                        type="button"
+                                        className="icon-btn"
+                                        onClick={searchClick}
+                                    >
+                                        <img src="./images/logo-icon/search-white.png" alt="search" />
+                                    </button>
+                                </li>
+                                <li>
+                                    <Link to="/login"><img src="./images/logo-icon/user-white.png" alt="" /></Link>
+                                </li>
+                                <li>
+                                    <Link to="/cart">
+                                        <img src="./images/logo-icon/cart-white.png" alt="" />
+                                        <span className="cart-num">0</span>
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
                 </div>
             </div>
 
-            <ul className={`scroll-menu ${isHover ? "" : lastScroll ? "active" : ""}`}>
+
+
+
+            <ul className={`scroll-menu ${!isSearchOpen && !isHover && lastScroll ? "active" : ""}`}>
                 {menus.map((menu, index) => (
                     <li key={`${menu.key}-${index}`}>
                         <Link to={menu.link}>
