bfc4f94a82d6b1bd5b7501418f8389332634609d
minjung1ee <lminjung9996@gmail.com>
2026-04-15 15:56:03 +0900

    서브 _ 매장안내 진행중

--- src/pages/StoreInfoComponents/InfoHeader.jsx
+++ src/pages/StoreInfoComponents/InfoHeader.jsx
@@ -15,18 +15,18 @@ export default function InfoHeader({
 }) {
     //tab 상태 저장
     //keyword, location
-    const [selectedSearchType, setSelectedSearchType] = useState("keyword");
-    console.log(selectedSearchType);
+    // const [selectedSearchType, setSelectedSearchType] = useState("keyword");
+    // console.log(selectedSearchType);
 
     return (
         <div className='store-control-header'>
             <ul className="tab">
-                <li onClick={() => setSelectedSearchType("keyword")} className={selectedSearchType === "keyword" ? "tab_active" : ""}><a href="#">직접검색</a></li>
-                <li onClick={() => setSelectedSearchType("location")} className={selectedSearchType === "location" ? "tab_active" : ""}><a href="#">지역검색</a></li>
+                <li onClick={() => setSelectedSearch("keyword")} className={selectedSearch === "keyword" ? "tab_active" : ""}><a href="#">직접검색</a></li>
+                <li onClick={() => setSelectedSearch("location")} className={selectedSearch === "location" ? "tab_active" : ""}><a href="#">지역검색</a></li>
             </ul>
             <div className="store-control-search-section">
                 {
-                    selectedSearchType === "keyword" ? (<KeywordSearch keyword={keyword} setKeyword={setKeyword} />)
+                    selectedSearch === "keyword" ? (<KeywordSearch keyword={keyword} setKeyword={setKeyword} />)
                         : (
                             <LocationSearch
 
