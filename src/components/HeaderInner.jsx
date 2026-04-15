00f10ecabccda94628e868c039777b2b385759b1
sungmin <okok0216@gmail.com>
2026-04-15 16:11:56 +0900

    search

--- src/components/HeaderInner.jsx
+++ src/components/HeaderInner.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useProductStore } from '../store/useProductStore'
 import { useAuthStore } from '../store/useAuthStore'
 import "./scss/headerinner.scss"
 import SearchDropdown from './SearchDropdown'
 
-const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick }) => {
+const HeaderInner = ({
+    onEnter,
+    userClick,
+    isHover,
+    isSearchOpen,
+    setIsSearchOpen,
+    searchClick
+}) => {
     const { menus } = useProductStore()
     const { user } = useAuthStore()
 
     const [lastScroll, setLastScroll] = useState(false)
-
 
     useEffect(() => {
         let lastScrollY = window.scrollY
 
         const AddMenuScroll = () => {
             const currentScrollY = window.scrollY
+
             if (currentScrollY === 0) {
                 setLastScroll(false)
             } else if (currentScrollY > lastScrollY) {
@@ -24,6 +31,7 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
             } else {
                 setLastScroll(true)
             }
+
             lastScrollY = currentScrollY
         }
 
@@ -33,8 +41,6 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
 
     return (
         <>
-
-
             <div className="header-inner">
                 <div className="header-left">
                     <div className="ham-btn" onMouseEnter={onEnter}>
@@ -43,7 +49,9 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
                 </div>
 
                 <h1 className="main-logo">
-                    <Link to="/"><img src="./images/logo-icon/main-logo-white.png" alt="" /></Link>
+                    <Link to="/">
+                        <img src="./images/logo-icon/main-logo-white.png" alt="" />
+                    </Link>
                 </h1>
 
                 <div className="header-right">
@@ -60,7 +68,7 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
                                     </button>
                                 </li>
                                 <li>
-                                    <button className='user-btn' onClick={userClick}>
+                                    <button className="user-btn" onClick={userClick}>
                                         <img src="./images/logo-icon/user-white.png" alt="" />
                                         <span>{user.name}</span>
                                     </button>
@@ -84,7 +92,9 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
                                     </button>
                                 </li>
                                 <li>
-                                    <Link to="/login"><img src="./images/logo-icon/user-white.png" alt="" /></Link>
+                                    <Link to="/login">
+                                        <img src="./images/logo-icon/user-white.png" alt="" />
+                                    </Link>
                                 </li>
                                 <li>
                                     <Link to="/cart">
@@ -97,7 +107,6 @@ const HeaderInner = ({ onEnter, userClick, isHover, isSearchOpen, searchClick })
                     </ul>
                 </div>
             </div>
-
 
 
 
