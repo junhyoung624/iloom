53b684eb39675a5189c9d79addd3242016a22157
junhyoung624 <mjhyoung0624@gmail.com>
2026-05-15 11:20:46 +0900

    ㅅㅈ..

--- src/components/QuickMenu.jsx
+++ src/components/QuickMenu.jsx
@@ -57,7 +57,7 @@ export default function QuickMenu() {
         setIsLoading(true);
 
         try {
-            const res = await fetch("http://localhost:4000/api/chat", {
+            const res = await fetch("/.netlify/functions/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message: userMessage, history: chatList }),
